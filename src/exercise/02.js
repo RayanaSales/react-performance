b70bffca6a7ae887a148a69e17379589246aa781
RayanaSales <rayanaasales@gmail.com>
2023-12-27 22:36:24 -0300

    Exercises 2 and 3

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,8 @@
 
 import * as React from 'react'
 import {useCombobox} from '../use-combobox'
-import {getItems} from '../filter-cities'
-import {useForceRerender} from '../utils'
+import {getItems} from '../workerized-filter-cities'
+import {useAsync, useForceRerender} from '../utils'
 
 function Menu({
   items,
@@ -60,9 +60,18 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = getItems(inputValue)
+  const {data: allItems, run} = useAsync({data: [], status: 'pending'})
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
   const items = allItems.slice(0, 100)
+
+  /** The React.useMemo for this case, will return the same if the inputValue still he same.
+   * if the inputValue changes, another call will be performed. Otherwise, same result is returned, without call again the heavy function.
+   *
+   * SHE SHOULD USE THIS FOR PURE FUNCTIONS - functions that dont change if the param persists.
+   */
+  // const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
 
   const {
     selectedItem,
